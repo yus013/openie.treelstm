7787ac9e75ed5d102bbf83467348c1468c23f9d9
NicoSensei <yujingshen96@gmail.com>
2018-01-22 18:34:21 +0800

    create vocab

---
+++ vocab.py
@@ -0,0 +1,68 @@
+import json
+
+class Vocab(object):
+    def __init__(self, filename, labels):  # default lower case
+        self.idx2wd = list()
+        self.wd2idx = dict()
+
+        self._load_file(filename)
+        self._add_specials(labels)
+        
+
+    def __len__(self):
+        return len(self.idx2wd)
+    
+    def _load_file(self, filename):
+        with open(filename, 'r') as f:
+            self.idx2wd = json.load(f)
+        self.update_idx()
+    
+    def _add_specials(self, labels):
+        for label in labels:
+            if label not in self.wd2idx:
+                i = len(self.idx2wd)
+                self.idx2wd.append(label)
+                self.wd2idx[label] = i
+            else:
+                raise KeyError
+    # end add specials
+    
+    def update_idx(self):
+        self.wd2idx = dict()
+        for i, wd in enumerate(self.wd2idx):
+            self.wd2idx[wd] = i
+    
+    def get_idx(self, wd):
+        return self.wd2idx[wd.lower()]
+
+    def get_wd(self, idx):
+        return self.idx2wd[idx]
+
+    def convert_to_idxs(self, wd_lst, UNK, BOS=None, EOS=None):
+        """
+        @param BOS: begin of sentence
+        @param EOS: end of sentence
+        """
+
+        idx_lst = list()  # word vector
+
+        if BOS is not None:
+            idx_lst.append(self.wd2idx[BOS])
+        
+        for wd in wd_lst:
+            idx_lst.append(self.wd2idx.get(wd.lower(), self.wd2idx[UNK]))
+        
+        if EOS is not None:
+            idx_lst.append(self.wd2idx[EOS])
+
+        return idx_lst
+
+    def convert_to_wds(self, idx_lst, stop=-1):
+        wd_lst = list()
+
+        for idx in idx_lst:
+            wd_lst.append(self.idx2wd[idx])
+            if idx == stop:
+                break
+        
+        return wd_lst
